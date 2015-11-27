fbc01f5f01162c3f5c6cd8dd91e1f56ad7750387
mioz <wei_0624@163.com>
2015-11-27 12:48:29 +0800

    add files

---
+++ src/UntiRollingFileAppenderAppender.java
@@ -0,0 +1,11 @@
+
+
+import org.apache.log4j.Priority;
+import org.apache.log4j.RollingFileAppender;
+
+public class UntiRollingFileAppenderAppender extends RollingFileAppender  {
+
+	public boolean isAsSevereAsThreshold(Priority priority) {
+		return !this.getThreshold().equals(priority);
+	}
+}
